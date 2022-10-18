d6a1cc01ec1e5e25d9d49a92a9968012a47aafc8
CarolinaFuenzalida <carolinapazfuenzalidapoblete@gmail.com>
2022-10-18 12:22:31 -0300

    devuelve cositas

--- index.js
+++ index.js
@@ -1,3 +1,142 @@
-module.exports = () => {
-  // ...
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+const colors = require("colors");
+
+//Parametro que ingresa User para ser examinado
+const ruta = process.argv[2];
+console.log(ruta.rainbow);
+
+//Comprueba si ruta existe, solo funciona con relativas
+const routeExist = (source) => {
+  if (fs.existsSync(source) === false) {
+    return "Ruta NO existe";
+  }
+  return "Ruta existe";
 };
+console.log(routeExist(ruta));
+
+// Transforma a relativa
+const getAbsoluteLink = (source) => {
+  if (path.isAbsolute(source) === false) {
+    return path.resolve(source);
+  }
+  return source;
+};
+console.log(getAbsoluteLink(ruta));
+
+// Comprobamos si es archivo
+const routeType = (source) => {
+  if (source.isFile() === true) {
+    return true;
+  }
+  return false;
+};
+
+// Leer archivos de tu ruta principal
+const routeFiles = fs.statSync(ruta);
+console.log("es archivo? " + routeType(routeFiles));
+
+// Lectura de directorio
+const readFolder = (directory) => fs.readdirSync(directory, "utf8");
+console.log(readFolder(ruta));
+
+// Filtrar archivos .md por extensión
+const fileExtensionMD = (file) => path.extname(file) === ".md";
+console.log("hola", fileExtensionMD(ruta));
+
+// Función que lee directorio y retorna los archivos.md
+let arrayOfMDFiles = [];
+const extractionFilesMD = (directory) => {
+  const dataDirectory = readFolder(directory);
+  let arrayFiles = dataDirectory.filter((element) => {
+    if (fileExtensionMD(element)) {
+      arrayOfMDFiles.push(element);
+      return element;
+    }
+  });
+  return arrayFiles;
+};
+console.log(extractionFilesMD(ruta));
+console.log("hola guardo array de elementos MD", arrayOfMDFiles);
+
+//Filtro de archivo md
+const dir = fs.readdirSync(ruta, { encoding: "utf8", flag: "r" });
+let array = [];
+function rute(dir) {
+  return (array = dir.filter((archivo) => {
+    return path.extname(archivo) === ".md";
+    //console.log(array);
+  }));
+}
+//Lee y extrae Links, verifica status de estos
+const arrayMd = rute(dir);
+const Url =
+  /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
+const exitos = [];
+const errores = [];
+let validos;
+let broken;
+function readMDLinks(paths) {
+  //console.log("ejecutando path");
+  paths.forEach((element) => {
+    const data = fs.readFileSync(element, { encoding: "utf8", flag: "r" });
+    //console.log("leyendo", data);
+    let findLinks = data.match(Url);
+    console.log(findLinks);
+    const arrfetch = findLinks.map((url) => {
+      return fetch(url);
+    });
+    console.log(arrfetch);
+    Promise.allSettled(arrfetch)
+      .then((result) => {
+        result.forEach((res) => {
+          if (res.status === "fulfilled") {
+            if (res.value.status === 200) {
+              console.log("Exito ", res.value?.status, res.value?.url);
+            } else {
+              console.log("Error ", res.value?.status, res.value?.url);
+            }
+            exitos.push({ status: res.value?.status, url: res.value?.url });
+          } else {
+            console.log("Error", res.reason);
+            errores.push({ error: res.reason });
+          }
+          validos = exitos.filter((url) => url.status === 200);
+          //console.log(validos.length);
+          broken = exitos.filter((url) => url.status !== 200);
+          //console.log(broken);
+        });
+      })
+      .finally(() => {
+        console.log("-------------------------------------------");
+        console.log(
+          "| total exito : ",
+          validos.length,
+          " | ",
+          "total errores: ",
+          broken.length,
+          " | "
+        );
+        console.log("-------------------------------------------");
+        console.log("|         Detalle Exito                   |");
+        console.log("-------------------------------------------");
+        exitos.forEach((exito) =>
+          console.log(`status : ${exito.status}  url : ${exito.url}`)
+        );
+      });
+  });
+}
+
+console.log(readMDLinks(arrayMd));
+
+// ESTA PARTE NO FUNCIONA AYUDA SOLO TOMABA PARAMETRO DADO POR README.MD
+/*const fileContents = fs.readFileSync(ruta, "utf8");
+const { metadata, content } = parseMD(fileContents);
+
+//se leen los links
+
+
+console.log(transformed); */
---
+++ prueba/READT.md
@@ -0,0 +1,12 @@
+
+## 2. Resumen del proyecto
+
+En este proyecto crearás una herramienta de línea de comando (CLI) así como tu
+propia librería (o biblioteca - library) en JavaScript.
+
+
+(http://community.laboratoria.la/t/modulos-librerias-paquetes-frameworks-cual-es-la-diferencia/175)
+(https://otra-cosa.net/algun-doc.html)
+(https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
+(https://nodejs.org/es/)
+(https://developers.google.com/v8/)
